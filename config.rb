4481353dc60b17873239eeac574688548c7bc24d
Gianluca Rigoletti <gianluca.rigoletti@gmail.com>
2016-03-29 13:16:26 +0200

    Update config.rb to development environment

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ require 'singularitygs'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "assets/css"
+css_dir = "assets/css/dev"
 sass_dir = "assets/sass"
 images_dir = "assets/images"
 javascripts_dir = "assets/javascripts"
