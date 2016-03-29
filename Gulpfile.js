ca0c1bb50455e7dc977927f5d210aa749e0dedf8
Gianluca Rigoletti <gianluca.rigoletti@gmail.com>
2016-03-29 13:16:02 +0200

    Update Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,36 +1,27 @@
 var gulp        = require('gulp');
-var browserSync = require('browser-sync');
 var compass     = require('gulp-compass');
-var reload      = browserSync.reload;
 
-// Start the server
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: "./"
-        }
-    });
-});
-
-
-// Compile SASS & auto-inject into browsers
+// Compile SASS & auto-inject into browsers for development
 gulp.task('compass', function () {
     return gulp.src('sass/*.sass')
         .pipe(compass({
 	      config_file: './config.rb',
-	      css: 'stylesheets',
-	      sass: 'sass'
+	      css: 'assets/css/dev',
+	      sass: 'assets/sass'
 	    }))
-        .pipe(browserSync.reload({stream:true}));
 });
 
-// Reload all Browsers
-gulp.task('bs-reload', function () {
-    browserSync.reload();
+// Compile SASS & auto-inject into browsers for production
+gulp.task('dist', function () {
+    return gulp.src('sass/*.sass')
+        .pipe(compass({
+          config_file: './config-dist.rb',
+          css: 'assets/css/dist',
+          sass: 'assets/sass'
+        }))
 });
 
 // Watch sass AND html files, doing different things with each.
-gulp.task('default', ['browser-sync', 'compass'], function () {
+gulp.task('default', ['compass'], function () {
     gulp.watch("sass/*.sass", ['compass']);
-    gulp.watch("*.html").on("change", browserSync.reload);
 });
